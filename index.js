0c97112612fc4e7ee9371b57730e9155fd54dff5
Abdus salam <salam.ads97@gmail.com>
2021-11-02 03:12:22 +0600

    cors error solved

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config()
 
 
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiptd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
