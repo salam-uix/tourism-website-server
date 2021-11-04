477016163c6af874267b653a6828be0fc563e014
Abdus salam <salam.ads97@gmail.com>
2021-11-04 06:04:07 +0600

    error issue

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
 
 
         // GET Single service
-        app.get('/services/:id', async (req, res) => {
+        app.get('/singleProduct/:id', async (req, res) => {
             const id = req.params.id;
             console.log('getting specific service', id);
             const query = { _id: ObjectId(id) };
